307522e36f361c5238d4205dce5b6d018d93aa4e
Romain Durade <durade.romain@gmail.com>
2024-04-11 12:41:39 +0200

    feat: add infraredCharacterRight sensor support

--- src/ArduRoomba.cpp
+++ src/ArduRoomba.cpp
@@ -51,6 +51,9 @@ bool ArduRoomba::_parseStreamBuffer(uint8_t *packets, int len, RoombaInfos *info
     case ARDUROOMBA_SENSOR_INFRAREDCHARACTERLEFT:
       infos->infraredCharacterLeft = (uint8_t)_parseOneByteStreamBuffer(packets, i);
       break;
+    case ARDUROOMBA_SENSOR_INFRAREDCHARACTERRIGHT:
+      infos->infraredCharacterRight = (uint8_t)_parseOneByteStreamBuffer(packets, i);
+      break;
     case ARDUROOMBA_SENSOR_DIRTDETECT:
       infos->dirtdetect = (int)_parseOneByteStreamBuffer(packets, i);
       break;
